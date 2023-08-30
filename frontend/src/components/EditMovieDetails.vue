83cdcbc533e447daf5f4f367ce0bbb196d2a525b
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-08-30 16:36:05 +0100

    Create EditMovieDetails component

---
+++ frontend/src/components/EditMovieDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="mt-16 mb-5">Edit Movie Details</h1>
+  <v-row no-gutters>
+    <!-- left column -->
+    <v-col xs="12" sm="6">
+      <MovieDescriptionForm :movie="movie" />
+      <MovieActorsForm :movie="movie"/>
+      <MovieRatingForm :movie="movie" />
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import MovieDescriptionForm from './MovieDescriptionForm.vue'
+import MovieActorsForm from './MovieActorsForm.vue'
+import MovieRatingForm from '../components/MovieRatingForm.vue'
+
+defineProps({
+  movie: {
+    type: Object,
+    required: true
+  }
+})
+</script>
