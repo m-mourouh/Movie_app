e348ec96ef29bce386c5f2858260434eb29f9934
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-08-30 12:46:19 +0100

    Movie router added

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import MovieViewSet
 
+router = DefaultRouter()
+router.register('movies', MovieViewSet, basename='movie')
 
 urlpatterns = [
     
 ]
+
+urlpatterns += router.urls
