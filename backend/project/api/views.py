f127b24487ebdf166e879a54d30511b440f19633
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-08-30 12:38:34 +0100

    Create Movie  viewSet

--- backend/project/api/views.py
+++ backend/project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import Movie
 
-# Create your views here.
+
+# Movie ViewSet
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
