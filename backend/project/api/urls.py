c520bcdcd9aa12cd09f9a7a0550adb85ecd790e0
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-08-30 13:01:05 +0100

    Review router added

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import MovieViewSet, ActorViewSet
+from .views import MovieViewSet, ActorViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register('movies', MovieViewSet, basename='movie')
 router.register('actors', ActorViewSet, basename='actors')
+router.register('reviews', ReviewViewSet, basename='review')
 
 urlpatterns = [
     
