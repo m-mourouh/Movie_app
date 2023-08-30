829e5dd46c85de0f02a7542e1cd7cf74b3ff491b
m-mourouh <mouhamedmourouh1999@gmail.com>
2023-08-30 12:51:44 +0100

    Create Actor viewSet

--- backend/project/api/views.py
+++ backend/project/api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import MovieSerializer
-from .models import Movie
+from .serializers import MovieSerializer, ActorSerializer
+from .models import Movie, Actor
 
 
 # Movie ViewSet
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    
+# Actor ViewSet
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
